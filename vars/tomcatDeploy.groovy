e6a20d5c990781dee4ab877beaccc06ac2c296fd
vijaybernad <vijay.solitude@gmail.com>
2021-05-20 20:17:00 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -3,7 +3,7 @@ def call(cred_id,user,ip){
       //rename the var file
      sh 'mv target/*.war target/myweb.war'
      //copy file to tomcat server
-     sh 'scp -o StrictHostKeyChecking=no target/myweb.war ${user}@${ip}:/opt/tomcat8/webapps'
+     sh "scp -o StrictHostKeyChecking=no target/myweb.war ${user}@${ip}:/opt/tomcat8/webapps"
      sh "ssh ${user}@${ip} /opt/tomcat8/bin/shutdown.sh"
      sh "ssh ${user}@${ip} /opt/tomcat8/bin/startup.sh"
   } 
